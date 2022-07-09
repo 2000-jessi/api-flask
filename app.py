3c04e399b568af77741d284ad195bc3839298034
2000-jessi <jessica2_bece@yahoo.com>
2022-07-09 13:40:58 -0500

    4 funciones api

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, send_file
 
 app = Flask(__name__)
 
 @app.route('/',methods=['GET'])
 def home():
-    return jsonify({"message": "Servidor en puerto"})
+    file = open("./spam-ham.txt","r")
+    spam= file.read(3)
+    ham= file.read(3)
+    return jsonify({"Spam": str(spam),"Ham":str(ham)})
 
+@app.route('/image',methods=['GET'])
+def image():
+    return send_file('./Correos.jpg',attachment_filename='Correos.jpg')
+
+@app.route('/prediction',methods=['GET'])
+def prediction():
+    file= open('./prediction.txt',"r")
+    prediction = file.read()
+    return jsonify({"Prediction": str(prediction)})
 
 if __name__ == '__main__':
     app.run(port=5000)
